8a145fbb8a562a8a791abca88a109cac3bf037d9
Jennifer Tieu <jennifer@yammm.com>
2022-10-30 22:25:20 -0500

    remove Ownable

--- contract.sol
+++ contract.sol
@@ -4,10 +4,9 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Alchemy is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+contract Alchemy is ERC721, ERC721Enumerable, ERC721URIStorage {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
